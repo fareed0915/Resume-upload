5e215072a92e4459440002d21808a2a509c3158a
Fareed Balogun <fareedbalogun@fareeds-mbp.mynetworksettings.com>
2023-09-13 13:06:06 -0500

    file upload and routing

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DriveService } from './drive.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  
+  constructor(
+    private http: HttpClient, 
+    private driveService: DriveService
+  ) {}
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+
+    // If you want to upload to your server
+    this.uploadToServer(file);
+
+    // If you want to upload to Google Drive
+    this.uploadToGoogleDrive(file);
+  }
+
+  private uploadToServer(file: File): void {
+    const formData = new FormData();
+    formData.append('resume', file, file.name);
+
+    this.http.post('/api/upload', formData).subscribe(
+      response => {
+        console.log('Upload to server successful', response);
+      },
+      error => {
+        console.error('Upload to server failed', error);
+      }
+    );
+  }
+
+  private uploadToGoogleDrive(file: File): void {
+    this.driveService.uploadFile(file)
+      .then(response => {
+        console.log('File uploaded to Google Drive successfully', response);
+      })
+      .catch(error => {
+        console.error('Error uploading file to Google Drive', error);
+      });
+  }
 }
